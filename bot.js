26a972f245786ad00a3822b0aef0e444bfb69245
Benjamin Clay <benj.clay@gmail.com>
2012-08-16 20:57:21 +0200

    Some refactoring... Need some tests :) AND MOAR REFACTORING FFS !

--- bot.js
+++ bot.js
@@ -29,11 +29,11 @@ client.addListener('pm', function (from, message) {
 function parseMsg(from, to, message) {
     if ('!' === message[0]) {
         var cmd = message.split(' ');
-        console.log(from + ' => ' + to + ': ' + message);
+        log(from + ' => ' + to + ': ' + message);
 
         // @TODO: ajouter la possibilité de préciser le chan sur lequel envoyer le message dans la commande
         // ex: !lastfm #channel ternel
-        if ('fatabot' == to) {
+        if (nconf.get('bot-name') == to) {
           to = '#eistibranlos';
         }
 
@@ -42,39 +42,46 @@ function parseMsg(from, to, message) {
          * @TODO : API Key in conf file
          */
         if ('!lastfm' == cmd[0] && undefined !== cmd[1]) {
-            var lastfm_url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&api_key=8c4df84b2b7c91d93a992dcccee83ba0&format=json"
-            lastfm_url = lastfm_url+'&user='+cmd[1];
+            LastFM_GetLastSong(cmd);
 
-            var options = {
-              host: 'ws.audioscrobbler.com',
-              port: 80,
-              path: '/2.0/?method=user.getrecenttracks&api_key=8c4df84b2b7c91d93a992dcccee83ba0&format=json&user='+cmd[1]
-            };
-
-            http.get(options, function(res) {
-                var json_data = '';
-                res.on('data', function (chunk) {
-                    json_data = json_data+chunk;
-                    try
-                    {
-                      var parsed_data = JSON.parse(json_data);
-                      getLastSong(to, cmd[1], parsed_data);
-                      json_data = '';
-                    }
-                    catch(e)
-                    {}
-                });
-
-                console.log("Got response: " + res.statusCode);
-
-            }).on('error', function(e) {
-              console.log("Got error: " + e.message);
-            });
         }
     }
 }
 
-function getLastSong(to, user, data) {
-    var track = data.recenttracks.track[0];
-    client.say(to, "[LastFM:"+user+"] "+track.artist['#text']+' - '+track.name);
+function LastFM_GetLastSong(to, user, data) {
+  var user = cmd[1];
+
+  var options = {
+    host: 'ws.audioscrobbler.com',
+    port: 80,
+    path: '/2.0/?method=user.getrecenttracks&api_key='+nconf.get('lastfm:api_key')+'&format=json&user='+user
+  };
+
+  http.get(options, function(res) {
+    var json_data = '';
+    res.on('data', function (chunk) {
+      json_data = json_data+chunk;
+      try
+      {
+        var parsed_data = JSON.parse(json_data);
+        var track = parsed_data.recenttracks.track[0];
+        client.say(to, "[LastFM:"+user+"] "+track.artist['#text']+' - '+track.name);
+        json_data = '';
+      }
+      catch(e)
+      {}
+    });
+
+    log("Got response: " + res.statusCode);
+
+  }).on('error', function(e) {
+    log("Got error: " + e.message);
+  });
+}
+
+
+function log(msg) {
+  if (nconf.get('debug')) {
+    console.log(msg);
+  }
 }
