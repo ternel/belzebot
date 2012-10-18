082ba5f861cb7120237c31fedf03a383d863a591
Joel Wurtz <jwurtz@jolicode.com>
2012-10-19 00:54:18 +0200

    Ajout cleverbot

---
+++ plugins/Cleverbot.js
@@ -0,0 +1,24 @@
+var Cleverbot = {};
+var nconf = require('nconf');
+var cleverBotLib = require('../lib/cleverbot.js');
+
+Cleverbot.support = function(message) {
+    return message.match(/^!cleverbot (.+?)/gi);
+};
+
+Cleverbot.handle = function(client, from, to, message) {
+    // @TODO: ajouter la possibilité de préciser le chan sur lequel
+    // envoyer le message dans la commande
+    // ex: !lastfm #channel ternel
+    if (nconf.get('bot-name') == to) {
+        to = '#eistibranlos';
+    }
+
+    match = message.match(/^!cleverbot (.+?)/gi);
+
+    cleverBotLib.write(match[1], function(data) {
+        client.say(to, data);
+    });
+};
+
+module.exports.Misc = Misc;
